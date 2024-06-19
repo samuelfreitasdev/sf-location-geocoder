fc636b95e3e6ae27507a4deac2b253b5bca17870
Samuel Freitas <samuelfreitasdev@gmail.com>
2024-06-19 01:00:40 -0300

    Refactor geocoder problem-solving logic in Vue
    The code refactor revolves around problem-solving logic in GeocoderSolver.vue. The commit introduces additional checks and conditions after fetching solutions. With this update, the application is given the ability to re-fetch solutions when it detects an 'ENQUEUED' or 'RUNNING' status, contributing to better solution handling. Also, helper functions for solving, terminating, and cleaning actions have been improved for better status management.

--- geocoder-webcli/src/views/GeocoderSolver/GeocoderSolver.vue
+++ geocoder-webcli/src/views/GeocoderSolver/GeocoderSolver.vue
@@ -4,18 +4,17 @@ import { GeocoderPageLayout, GeocoderSolverPanelLayout } from '../../layout'
 import { computed, ref } from 'vue'
 import { useRoute } from 'vue-router'
 import SolverPanel from './SolverPanel.vue'
-import { type GeocoderProblem, GeocoderSolution } from '../../api'
-import { useFetch, watchOnce } from '@vueuse/core'
+import { type GeocoderProblem, GeocoderSolution, type GeocoderSolutionRequest } from '../../api'
+import { type AfterFetchContext, useFetch, watchOnce } from '@vueuse/core'
 import SolverMap from '../../views/GeocoderSolver/SolverMap.vue'
 
 const route = useRoute()
 
 const solverStatus = ref<string | null>(null)
-const solution = ref<GeocoderSolution | null>(null)
-// const problem = ref<GeocoderProblem | null>(null)
 const selectedSolver = ref<string>('')
 
-// const solutionPanelUrl = computed(() => `/api/solver/${route.params.id}/solution`)
+const solutionPanelUrl = computed(() => `/api/solver/${route.params.id}/solution`)
+const refetchSolution = ref(true)
 const solverNamesUrl = ref('/api/solver/solver-names')
 
 const problemUrl = computed(() => `/api/problems/${route.params.id}`)
@@ -25,44 +24,55 @@ const cleanUrl = computed(() => `/api/solver/${route.params.id}/clean`)
 
 const { data: problem } = useFetch(problemUrl, { immediate: true }).get().json<GeocoderProblem>()
 
-// const {
-// 	isFetching,
-// 	error,
-// 	data: solutionPanel,
-// } = useFetch(solutionPanelUrl).get().json<GeocoderSolutionRequest>()
+const { error, execute: fetchProblem, data: solution } = useFetch(solutionPanelUrl, {
+	refetch: refetchSolution, afterFetch: afterFetchSolution, immediate: true,
+}).get().json<GeocoderSolutionRequest>()
 
-const {
-	isFetching,
-	error,
-	data: solvers,
-} = useFetch(solverNamesUrl, { initialData: [], immediate: true }).get().json<string[]>()
+const { isFetching, data: solvers } = useFetch(solverNamesUrl, {
+	initialData: [],
+	immediate: true,
+}).get().json<string[]>()
 
 const { data: solveStatus, execute: solve } = useFetch(solveUrl, { immediate: false }).post().json<string>()
 const { execute: terminate } = useFetch(terminateUrl, { immediate: false }).post().json<string>()
 const { execute: clean } = useFetch(cleanUrl, { immediate: false }).post().json<string>()
 
+function afterFetchSolution(ctx: AfterFetchContext<GeocoderSolutionRequest>) {
+	refetchSolution.value = ctx.data?.status === 'ENQUEUED' || ctx.data?.status === 'RUNNING'
+	solverStatus.value = ctx.data?.status || 'NOT_SOLVED'
+
+	if (refetchSolution.value) {
+		setTimeout(() => fetchProblem(), 1000)
+	}
+
+	return ctx
+}
+
 async function solveAction() {
 	if (problem.value) {
 		await solve()
-		solverStatus.value = solveStatus.value || null
+		solverStatus.value = solveStatus.value || 'NOT_SOLVED'
+		await fetchProblem()
 	}
 }
 
 async function terminateAction() {
 	if (problem.value) {
 		await terminate()
+		solverStatus.value = 'TERMINATED'
 	}
 }
 
 async function cleanAction() {
 	if (problem.value) {
 		await clean()
+		solverStatus.value = 'NOT_SOLVED'
 	}
 }
 
 watchOnce(solvers, () => {
-	selectedSolver.value = (solvers.value && solvers.value[0]) || "";
-});
+	selectedSolver.value = (solvers.value && solvers.value[0]) || ''
+})
 </script>
 
 <template>
@@ -71,7 +81,7 @@ watchOnce(solvers, () => {
 			<template #menu>
 				<solver-panel
 					v-model:selected-solver="selectedSolver"
-					:solution="solution"
+					:solution="solution?.solution as GeocoderSolution | null"
 					:problem="problem"
 					:solvers="solvers || []"
 					:solver-status="solverStatus"
@@ -84,7 +94,7 @@ watchOnce(solvers, () => {
 			<template #main>
 				<solver-map
 					v-if="(problem?.points || []).length > 0"
-					:solution="solution"
+					:solution="solution?.solution as GeocoderSolution | null"
 					:problem="problem"
 				/>
 			</template>
