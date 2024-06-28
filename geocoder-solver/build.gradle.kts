7c475eb5aa0535e63548dc613888e75b3a8a2488
Samuel Freitas <samuelfreitasdev@gmail.com>
2024-06-28 01:15:45 -0300

    Add new dependencies to geocoder-solver build script
    Two new dependencies were added to the geocoder-solver's build.gradle.kts script: commons-math3 from Apache Commons and jfreechart. These new libraries are necessary to provide mathematical functions and charting capabilities respectively for the geocoder-solver project.

--- geocoder-solver/build.gradle.kts
+++ geocoder-solver/build.gradle.kts
@@ -5,7 +5,10 @@ plugins {
 
 dependencies {
 	implementation(project(":geocoder-core"))
-//	api(libs.jsprit.core)
+
+	implementation("org.apache.commons:commons-math3:3.6")
+//	implementation("org.knowm.xchart:xchart:3.8.8")
+	implementation("org.jfree:jfreechart:1.5.5")
 
 	runtimeOnly(libs.slf4j)
 }
