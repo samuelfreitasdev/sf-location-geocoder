02478d9d7921823b8ff5301f358666d8e94d1f28
Samuel Freitas <samuelfreitasdev@gmail.com>
2024-08-22 00:10:14 -0300

    Add jvmArgs block to bootRun configuration
    This change introduces a commented-out jvmArgs block in the bootRun configuration of the build script. It includes various module and package access options that could be useful for future debugging or customization needs.

--- geocoder-app/build.gradle.kts
+++ geocoder-app/build.gradle.kts
@@ -71,6 +71,17 @@ tasks {
 			logger.quiet("Cli Resources: move from $webCliOrigin to $webCliDest")
 		}
 	}
+
+	bootRun {
+		jvmArgs(
+//			"-add-modules java.se",
+//			"-add-exports java.base/jdk.internal.ref=ALL-UNNAMED",
+//			"-add-opens java.base/java.lang=ALL-UNNAMED",
+//			"-add-opens java.base/sun.nio.ch=ALL-UNNAMED",
+//			"-add-opens java.management/sun.management=ALL-UNNAMED",
+//			"-add-opens jdk.management/com.sun.management.internal=ALL-UNNAMED"
+		)
+	}
 }
 
 configure<GitPropertiesPluginExtension> {
